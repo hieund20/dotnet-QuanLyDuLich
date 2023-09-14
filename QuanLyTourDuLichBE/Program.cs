b37deea9f4be3c25fafbb16f3da77fa79913ea21
hieund20 <hieund898@gmail.com>
2023-09-14 21:49:47 +0700

    add Swagger Bearer

--- QuanLyTourDuLichBE/Program.cs
+++ QuanLyTourDuLichBE/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
 using QuanLyTourDuLichBE.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,8 +20,48 @@ var configuration = builder.Configuration;
 builder.Services.AddDbContext<QLTourDuLichContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("QlTourDuLichConnectionString")));
 
+//
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "QuanLyTourDuLich API", Version = "v1" });
+
+    // Define the security scheme (Bearer token)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    // Define the security requirement (the policy)
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+//Add Authentication service
+builder.Services.AddAuthentication()
+    .AddJwtBearer()
+    .AddJwtBearer("LocalAuthIssuer");
+//Add Authorization service
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+//Use CORS middleware
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,6 +72,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
